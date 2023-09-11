cecbba4473c163c9e9d5adf98ea2c7075222cf0c
Munsif-Raza-T <76085202+Munsif-Raza-T@users.noreply.github.com>
2023-09-11 10:03:14 +0500

    All done

---
+++ README.md
@@ -0,0 +1,58 @@
+# Self-Improvement Guru 📚
+
+ Self-Improvement Guru is an AI-powered chatbot designed to provide accurate and prompt responses to all your queries related to Self-improvement. Built using Streamlit and powered by OpenAI's model, Self-Improvement Guru aims to make accessing self-improvement information convenient and efficient.
+
+## Features
+
+- **Personalized Self-Improvement:** Self-Improvement Guru is designed to cater to your unique needs and goals. Whether you're striving for better mental health, enhanced productivity, stronger relationships, or personal fulfillment, our AI chatbot tailors its advice to align with your aspirations.
+
+- **Expertise at Your Fingertips:** Backed by a wealth of knowledge and expertly curated resources, Self-Improvement Guru offers evidence-based advice and strategies across a wide range of self-improvement topics. From mindfulness and time management to goal setting and communication skills, you'll find valuable insights to propel your personal growth journey.
+
+- **24/7 Accessibility:** Self-Improvement Guru is available to assist you around the clock. Whenever you need inspiration, guidance, or a friendly push in the right direction, our AI chatbot is there to provide support, ensuring that you stay motivated and on track with your self-improvement goals.
+
+- **Interactive Conversations:** Engage in meaningful and interactive conversations with Self-Improvement Guru. Ask questions, seek advice, or simply have a friendly chat to boost your mood and motivation. Our chatbot is designed to make your self-improvement journey enjoyable and engaging.
+
+- **Data Privacy:** We take your privacy seriously. Self-Improvement Guru operates with strict data security measures in place to ensure that your personal information and conversations remain confidential.
+
+## How It Works
+
+Self-Improvement Guru is a revolutionary AI chatbot powered by state-of-the-art technology, including the OpenAI API, and enriched with custom data extracted from three renowned YouTube channels focused on self-improvement. The chatbot processes your input, understands the context, and generates relevant and informative responses.
+
+## Getting Started
+
+To run CSV-AI, follow these steps:
+
+1. Clone this repository to your local machine.
+
+```bash
+git clone https://github.com/Munsif-Raza-T/Self-Improvement-Guru.git
+```
+2. Install the required dependencies.
+```bash 
+pip install -r requirements.txt
+```
+3. Replace your OpenAI GPT-3 API key in the secrets.toml file.
+4. Run the Streamlit app.
+```bash 
+streamlit run app.py
+```
+This command will launch the Self-Improvement Guru app in your default web browser. 
+5. Interact with Self-Improvement Guru by typing your queries and receiving instant responses related to Self-Improvement.
+
+<img src="selfimpguru.PNG" alt="App Homepage">
+
+## Contributions and Improvements
+Self-Improvement Guru is an open-source project, and contributions are welcome! If you'd like to add new features, improve the chatbot's responses, or enhance the user experience, feel free to submit pull requests.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+---
+## **A project by Munsif Raza**
+
+[![Follow](https://img.shields.io/badge/LinkedIn-0A66C2.svg?style=for-the-badge&logo=LinkedIn&logoColor=white)](https://www.linkedin.com/in/munsifraza/)
+</br>
+Email: munsifrazaofficial@gmail.com
+</br>
+
+Feel free to use, modify, and share Self-Improvement Guru to enhance the accessibility of Self-Improvement information. If you have any questions or suggestions, please feel free to reach out to us. Happy querying with Self-Improvement Guru!
---
+++ app.py
@@ -0,0 +1,138 @@
+import streamlit as st
+import os
+import time
+import pinecone
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.llm import LLMChain
+from langchain.callbacks import get_openai_callback
+from langchain.chains import RetrievalQA
+
+
+st.set_page_config(
+    page_title="Self-improvement Guru",
+    page_icon="📚",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.sidebar.image("growth.png", width=190)
+
+with st.sidebar:
+    st.markdown("""# Welcome to Self-Improvement Guru""")
+    st.markdown(
+        "Unlock your full potential and embark on a transformative journey of self-improvement with our revolutionary chatbot, the \"Self-Improvement Guru.\" "
+        )
+    st.markdown(
+        "Designed to be your dedicated companion on the path to personal growth, this innovative AI-powered chatbot is here to provide guidance, support, and motivation for individuals seeking to enhance various aspects of their lives.🧑📚💪🥇\n"
+    )
+    st.markdown("---")
+    st.markdown("A project by Munsif Raza")
+    st.markdown("""[![Follow](https://img.shields.io/badge/LinkedIn-0A66C2.svg?style=for-the-badge&logo=LinkedIn&logoColor=white)](https://www.linkedin.com/in/munsifraza/)""")
+  
+# Getting the OpenAI API key from Streamlit Secrets
+openai_api_key = st.secrets.secrets.OPENAI_API_KEY
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
+# Getting the Pinecone API key and environment from Streamlit Secrets
+PINECONE_API_KEY = st.secrets.secrets.PINECONE_API_KEY
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+PINECONE_ENV = st.secrets.secrets.PINECONE_ENV
+os.environ["PINECONE_ENV"] = PINECONE_ENV
+# Initialize Pinecone with API key and environment
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+
+
+embeddings = OpenAIEmbeddings()
+model_name = "gpt-3.5-turbo-16k"
+text_field = "text"
+
+@st.cache_resource
+def ret():
+    # load a Pinecone index
+    index = pinecone.Index("self-improvement")
+    time.sleep(5)
+    db = Pinecone(index, embeddings.embed_query, text_field)
+    return db
+
+@st.cache_resource
+def init_memory():
+    return ConversationBufferWindowMemory(
+                                        k=2, 
+                                        memory_key="chat_history", 
+                                        return_messages=True,
+                                        verbose=True)
+
+memory = init_memory()
+
+_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a 
+standalone question without changing the content in given question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+condense_question_prompt_template = PromptTemplate.from_template(_template)
+
+prompt_template = """You are helpful information giving QA System and make sure you don't answer anything not related to following context. 
+You are always provide useful information & details available in the given context. Use the following pieces of context to answer the question at the end. 
+Also check chat history if question can be answered from it or question asked about previous history. If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+
+{context}
+Chat History: {chat_history}
+Question: {question}
+Answer:"""
+
+qa_prompt = PromptTemplate(
+    template=prompt_template, input_variables=["context", "chat_history","question"]
+)
+
+db = ret()
+
+#@st.cache_resource
+def conversational_chat(query):
+    llm = ChatOpenAI(model_name = model_name)
+    question_generator = LLMChain(llm=llm, prompt=condense_question_prompt_template, memory=memory, verbose=True)
+    doc_chain = load_qa_chain(llm, chain_type="stuff", prompt=qa_prompt, verbose=True)
+    agent = ConversationalRetrievalChain(
+        retriever=db.as_retriever(search_kwargs={'k': 6}),
+        question_generator=question_generator,
+        combine_docs_chain=doc_chain,
+        memory=memory,
+        verbose=True,
+    )
+
+    return agent
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+if prompt := st.chat_input():
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content":prompt, "avatar": "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"})
+    # st.chat_message("user").write(prompt)
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        agent = conversational_chat(prompt)
+        with st.spinner("Thinking..."):
+            with get_openai_callback() as cb:
+                response = agent({'question': prompt, 'chat_history': st.session_state.chat_history})#agent({"query": prompt})#conversational_chat(prompt)#
+                st.session_state.chat_history.append((prompt, response["answer"]))
+                message_placeholder.markdown(response["answer"])
+                st.session_state.messages.append({"role": "assistant", "content": response["answer"], "avatar": "https://drive.google.com/file/d/1SASLiowvEykNjU41AkH3uOkSEXHAB4m0/view?usp=drive_link"})
+            
